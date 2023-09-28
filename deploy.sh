88ca724a13f10133e727d2d886fbc72e115a0383
NerfedWizward <loelnelson@gmail.com>
2023-09-27 22:47:36 -0500

    Freestyling a solution

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,6 @@ mvn clean install
 echo "Building project"
 mvn clean package
 echo "Deploying project"
+scp -r src/main/resource/* loel@172.233.212.30
+scp loel@172.233.212.30:/var/www/172.233.212.30/applications.properties loel@172.233.212.30:/ppmtoolBeta/src/main/resources/
 java -Dserver.port=$PORT $JAVA_OPTS -jar target/PPMTool-0.0.1-SNAPSHOT.jar
